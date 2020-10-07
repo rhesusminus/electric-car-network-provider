0afb06168fbbe5f1a2b238b55febc963bd4d3e35
Erik Niilivirta <erik.niilivirta@gmail.com>
2020-10-07 16:35:50 +0300

    client initial changes

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/App.css'
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">hello world</header>
+    </div>
+  )
+}
