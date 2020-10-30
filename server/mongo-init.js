1bca1783f46038f39edaea6b9dd037139088e7d1
Erik Niilivirta <erik.niilivirta@gmail.com>
2020-10-30 23:42:31 +0200

    remove extra files

--- server/mongo-init.js
+++
@@ -1,7 +0,0 @@
-databases = ['users', 'stations']
-
-databases.forEach((db, index) => {
-  db = db.getSiblingDB(databases[index])
-
-  db.createUser({ user: 'admin', pwd: 'secret', roles: ['readWrite'] })
-})
